4e63548b5b5cb7a0de6fd521ba4e86425f10261d
Alex <alex_rodriguez_apps1ma1819@cev.com>
2019-12-11 14:13:08 +0100

    try catch done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,34 +27,41 @@ class UserController extends Controller
     {
         $data = ['email' => $request->email];
         $user = User::where('email', $request->email)->first();
-        if ($user) {
-            if ($user->password == $request->password) {
-                $token = new Token($data);
-                $encoded_token = $token->set_token($user->email);
-                return response()->json([
-                    'token' => $encoded_token
-                ], 200);
-            } else {
-                return response()->json([
-                    'message' => "incorrect password"
-                ], 401);
-            }
-        } else {
+        
+        try {
+                if ($user->password == $request->password) {
+                    $token = new Token($data);
+                    $encoded_token = $token->set_token($user->email);
+                    return response()->json([
+                        'token' => $encoded_token
+                    ], 200);
+                } else {
+                    return response()->json([
+                        'message' => "incorrect password"
+                    ], 401);
+                }
+        } catch (\Throwable $th) {
             return response()->json([
                 'message' => "incorrect email"
             ], 401);
-        }
+        } 
     }
 
     public function lend(Request $request)
     {
-        $token1 = new Token();
-        $token_decoded = $token1->decode_token($request->header('Authorization'));
-        $mail = $token_decoded;
-        $user = User::where('email', $mail)->first();
-        $user->books()->attach($request->book_id);
-        return response()->json([
-            "message" => "book lend correctly"
+            try {
+                $token1 = new Token();
+                $token_decoded = $token1->decode_token($request->header('Authorization'));
+                $mail = $token_decoded;
+                $user = User::where('email', $mail)->first();
+                $user->books()->attach($request->book_id);
+                return response()->json([
+                    "message" => "book lend correctly"
         ], 200);
+                } catch (\Throwable $th) {
+                return response()->json([
+                    'message' => "book not found"
+                ], 401);
+            } 
     }
 }
